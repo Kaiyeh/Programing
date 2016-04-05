24c4ed7ff75dbd14001b7d9b7f378587f07d34b1
Kaiyeh <kaiyeh.liu@gmail.com>
2016-04-05 10:39:26 +0800

    4/5

---
+++ C++/The-N-th-Element.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+int main(){
+using namespace std;
+int M,i;
+long long p,q,r,x,y,z,N;
+long long be1,af1,be2,af2,cr1,cr2;
+cin >> M;
+while(--M){
+    cin >> p >> q >> r >> x >> y >> z >> N;
+    be1=0;
+    be2=0
+    af1=N+1;
+    af2=N+1;
+    cr1=N-1/2;
+    cr2=N-1/2;
+
+    ///cr1+cr2==N-3&&(cr1==0||cr2==0||cr1==N||cr2==N)
+
+    while(1){
+
+
+
+
+
+
+    }
+
+}
+
+
+
+
+
+
+
+}
